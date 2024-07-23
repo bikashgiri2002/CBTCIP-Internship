11a101c056dd0a24d4d69555fc6c1e61d2087afd
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-07-24 00:48:17 +0530

    internship

---
+++ UnitConverter/app/src/main/java/com/devbiku/unitconverter/KgToPound.java
@@ -0,0 +1,42 @@
+package com.devbiku.unitconverter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class KgToPound extends AppCompatActivity {
+    Button calculate;
+    EditText inputText;
+    TextView outputText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_kg_to_pound);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        calculate = findViewById(R.id.btn_calculate);
+        inputText = findViewById(R.id.input);
+        outputText = findViewById(R.id.output);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double input = Double.parseDouble(String.valueOf(inputText.getText()));
+                double output = input * 2.2046 ;
+                outputText.setText(input+" Kg is "+String.valueOf(output)+" Pound");
+            }
+        });
+    }
+}
---
+++ UnitConverter/app/src/main/java/com/devbiku/unitconverter/MainActivity.java
@@ -0,0 +1,64 @@
+package com.devbiku.unitconverter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    CardView c1,c2,c3,c4,c5,c6;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        c1 = findViewById(R.id.kg_to_pound);
+        c2 = findViewById(R.id.meter_to_feet);
+        c3 = findViewById(R.id.feet_to_inch);
+        c4 = findViewById(R.id.celsius_to_fahrenheit);
+        c5 = findViewById(R.id.celsius_to_kelvin);
+        c6 = findViewById(R.id.fahrenheit_to_kelvin);
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1 = new Intent(MainActivity.this , KgToPound.class);
+                startActivity(i1);
+            }
+        });
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2 = new Intent(MainActivity.this, MeterToFeet.class);
+                startActivity(i2);
+            }
+        });
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i3 = new Intent(MainActivity.this, FeetToInch.class);
+                startActivity(i3);
+            }
+        });
+    }
+}
